3d8323b84a818ba89df20cf7b3cc86ab97bb377e
Hoaper <beeboplay@gmail.com>
2023-10-23 11:45:08 +0600

    All changes done

--- js/countdown.js
+++ js/countdown.js
@@ -1,21 +1,35 @@
-const countDownDate = new Date(2023, 10, 18).getTime();
 const countdown_body = document.querySelector('#countdown-body');
+const time_input_hours = document.querySelector('#time_input_hours');
+const time_input_mins = document.querySelector('#time_input_mins');
+const time_btn = document.querySelector('#time_btn');
+let future = NaN
 
-setInterval(function() {
-
-  let now = new Date().getTime();
-  let ration = countDownDate - now;
-
-  let days = Math.floor(ration / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((ration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((ration % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((ration % (1000 * 60)) / 1000);
-
-  countdown_body.innerHTML = "УСПЕЙ ПОМОЧЬ! " + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (ration < 0) {
-    clearInterval(x);
-    countdown_body.innerHTML = "EXPIRED";
+function getCurrentTimePlus() {
+  if (future) { 
+    return future;
   }
-}, 1000);
+  future = new Date();
+  future.setHours(future.getHours() + parseInt(time_input_hours.value), future.getMinutes() + parseInt(time_input_mins.value))
+  return future;
+}
+
+time_btn.addEventListener("click", () => setInterval(function() {
+    let nowMS = new Date().getTime();
+    
+    futureTime = getCurrentTimePlus();
+    let ration = futureTime - nowMS;
+
+    let days = Math.floor(ration / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((ration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((ration % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((ration % (1000 * 60)) / 1000);
+
+    countdown_body.innerHTML = "УСПЕЙ ПОМОЧЬ! " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (ration < 0) {
+      clearInterval(x);
+      countdown_body.innerHTML = "EXPIRED";
+    }
+  }, 1000)
+);
---
+++ js/feedback.js
@@ -0,0 +1,5 @@
+const msg_btn = document.querySelector('#message_btn');
+
+msg_btn.addEventListener('click', () => {
+    alert("THANKS FOR FEED BACK!")
+})
